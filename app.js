fcde747a4d9ae21ed956416332a48e775a8ba06a
unknown <gauravkubetkar76@gmail.com>
2021-04-10 19:54:20 +0530

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Welcome!'));
+app.get('/', (req, res) => res.send('We have Successfully added a review stage with manual approval to our continuous delivery pipeline!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
